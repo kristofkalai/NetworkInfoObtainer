42d4ac5f0e75cb12a2545ac909874fb56200f293
Kristóf Kálai <kristof.kalai@supercharge.io>
2024-01-28 21:33:10 +0100

    feat: add example app

---
+++ Example/Example/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Kristóf Kálai on 28/01/2024.
+//
+
+import SwiftUI
+import Reachability
+import NetworkInfoObtainer
+
+extension Reachability: NetworkReachability {}
+
+struct ContentView: View {
+    private let notifier = IpAddressChangeNotifierImpl(
+        networkInfoObtainer: NetworkInfoObtainerImpl()
+    )
+    private let provider = NetworkInfoProviderImpl(
+        ipAddressChangeNotifier: IpAddressChangeNotifierImpl(
+            networkInfoObtainer: NetworkInfoObtainerImpl()
+        )
+    )
+    private let reachabilityProvider = NetworkInfoProviderImpl(
+        ipAddressChangeNotifier: IpAddressChangeNotifierImpl(
+            networkInfoObtainer: NetworkInfoObtainerImpl()
+        ),
+        networkReachability: try? Reachability()
+    )
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            notifier.startNotifier(interval: 5)
+            notifier.ipAddressDidChange = {
+                print("Notifier info: \($0)")
+            }
+
+            try? provider.startNotifier()
+            provider.ipAddressDidChange = {
+                print("Provider info: \($0)")
+            }
+
+            try? reachabilityProvider.startNotifier()
+            reachabilityProvider.ipAddressDidChange = {
+                print("ReachabilityProvider info: \($0)")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -0,0 +1,10 @@
+//
+//  ExampleUITests.swift
+//  ExampleUITests
+//
+//  Created by Kristóf Kálai on 28/01/2024.
+//
+
+import XCTest
+
+final class ExampleUITests: XCTestCase {}
