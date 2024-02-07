36f9dad95fce6a8ee663eaab44f7a30ced0ebf8f
Kristóf Kálai <kristof.kalai@supercharge.io>
2024-02-07 22:06:51 +0100

    feat: add NetworkReachabilityImpl

---
+++ Sources/NetworkInfoObtainer/NetworkReachability/NetworkReachabilityImpl.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//
+//
+//  Created by Kristóf Kálai on 07/02/2024.
+//
+
+import Foundation
+import Network
+
+public final class NetworkReachabilityImpl {
+    public var whenReachable: ((NetworkReachabilityImpl) -> Void)?
+    public var whenUnreachable: ((NetworkReachabilityImpl) -> Void)?
+
+    private let monitor = NWPathMonitor()
+
+    init() {
+        monitor.pathUpdateHandler = { [weak self] in
+            guard let self else { return }
+
+            if $0.status == .satisfied {
+                whenReachable?(self)
+            } else {
+                whenUnreachable?(self)
+            }
+        }
+    }
+}
+
+extension NetworkReachabilityImpl: NetworkReachability {
+    public func startNotifier() {
+        monitor.start(queue: .global())
+    }
+
+    public func stopNotifier() {
+        monitor.cancel()
+    }
+}
